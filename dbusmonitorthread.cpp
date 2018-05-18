c09cacc65043dcec4ee9c684c0ad61b34f302af7
Alexey Minnekhanov <a.minnekhanov@omprussia.ru>
2018-05-18 11:05:32 +0300

    Add class

---
+++ dbusmonitorthread.cpp
@@ -0,0 +1,6 @@
+#include "dbusmonitorthread.h"
+
+DBusMonitorThread::DBusMonitorThread()
+{
+
+}
---
+++ dbusmonitorthread.h
@@ -0,0 +1,12 @@
+#ifndef DBUSMONITORTHREAD_H
+#define DBUSMONITORTHREAD_H
+
+#include <QObject>
+
+class DBusMonitorThread
+{
+public:
+    DBusMonitorThread();
+};
+
+#endif // DBUSMONITORTHREAD_H
